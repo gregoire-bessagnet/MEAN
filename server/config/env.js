8dff30c6e3da9c55895b32561976fcdcdf5ee7ad
Gregoire Bessagnet <gregoire.bessagnet@inbox.com>
2017-06-27 16:20:25 +0200

    added mongodDB uri to env

--- server/config/env.js
+++ server/config/env.js
@@ -14,7 +14,7 @@ let config = {
   },
 
   production: {
-    db: process.env.MONGODB_URI || 'mongodb://heroku_k38xhh52:3fpqr8knth6fs4q0pr5r605qco@ds131512.mlab.com:31512/heroku_k38xhh52',
+    db: process.env.MONGODB_URI || 'mongodb://heroku_q38lp2xt:epfgp8nat2u9o6jqbdb01tp9ep@ds139082.mlab.com:39082/heroku_q38lp2xt',
     cookieToken: 'mean-token',
     jwtSecret: 'cJbdB3t$',
     facebookAuth: {
